68efbc32623ea16d5d04b5f535e820a8854b88d1
Elano N. Caitano <nunes.elano14@gmail.com>
2023-02-07 21:06:51 -0300

    Criação do DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using ToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder) => 
+        optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+
+  }
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+namespace ToDo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+        public bool Done { get; set; }       
+    }
+}
